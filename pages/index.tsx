0befc2b261b1faadd955acbaf471e9a8ffe16f4b
666 <48902132+LacHex@users.noreply.github.com>
2022-10-19 20:16:20 +0800

    Add theme colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,7 @@ const HomePage: NextPage = () => {
   return (
     <main className="grid place-content-center min-h-screen py-16">
       <h1 className="text-6xl font-medium">
-        Welcome to{' '}
-        <span className="text-transparent bg-clip-text text-red-600">
-          7 News
-        </span>
+        Welcome to <span className="text-primary">7 News</span>
       </h1>
     </main>
   )
