ccabffa95240cd3e7ef71baa3bf9a97ef5b3bd4a
LacHex <48902132+LacHex@users.noreply.github.com>
2022-10-20 20:27:42 +0800

    Move `calcTimeAgo()` to helpers  directory

--- components/ui/ArticleCard.tsx
+++ components/ui/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { calcTimeAgo } from '@lib/helpers'
 
 interface ArticleProps {
   id: number
@@ -31,29 +32,6 @@ const ArticleCard = ({
       </a>
     </Link>
   )
-}
-
-const calcTimeAgo = (date: string): string => {
-  const seconds = Math.floor(
-    (new Date().getTime() - new Date(date).getTime()) / 1000
-  )
-  const years = Math.floor(seconds / 31536000)
-  const months = Math.floor(seconds / 2592000)
-  const days = Math.floor(seconds / 86400)
-  const hours = Math.floor(seconds / 3600)
-  const minutes = Math.floor(seconds / 60)
-
-  return years > 0
-    ? `${years} year${years > 1 ? 's' : ''} ago`
-    : months > 0
-    ? `${months} month${months > 1 ? 's' : ''} ago`
-    : days > 0
-    ? `${days} day${days > 1 ? 's' : ''} ago`
-    : hours > 0
-    ? `${hours} hour${hours > 1 ? 's' : ''} ago`
-    : minutes > 0
-    ? `${minutes} minute${minutes > 1 ? 's' : ''} ago`
-    : 'Just now'
 }
 
 export default ArticleCard
---
+++ lib/helpers/index.ts
@@ -0,0 +1,1 @@
+export { default as calcTimeAgo } from './calcTimeAgo'
