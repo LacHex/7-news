ac941e452600decd348911f04e1fc35f14c29fbd
LacHex <48902132+LacHex@users.noreply.github.com>
2022-10-20 20:12:55 +0800

    Add missing font import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ export default class CustomDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&family=Poppins:wght@400;500&display=swap"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
